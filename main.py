867c88193ff3932115baf3cd25d1aee40dead415
Alex Goodenbour <alg107@uclive.ac.nz>
2019-12-12 21:24:26 +1300

    second

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class Body:
             dist = other.pos-self.pos
             distMag = np.sqrt(dist.dot(dist))
             F = F + (G*self.mass*other.mass/distMag**3)*dist
-            if distMag < 10000000000:
+            if distMag < 1e10:
                 F = 0.0
                 if not crashed:
                     print("Crash!")
@@ -60,20 +60,28 @@ def update_body_list(blist):
         b.update_pos(step, np.delete(blist, idx))
     return blist
 
+def calc_COM(blist):
+    totalMass = np.sum([a.mass for a in blist])
+    moms = [a.mass*a.pos for a in blist]
+    totalMom = [0.0, 0.0, 0.0]
+    for m in moms:
+        totalMom += m
+    return totalMom/totalMass
+
 earth_sun_sys = np.array([
 
     Body(sun_m,
         (0.0, 0.0, 0.0),
-        (0.0, 0.0, 0.0)
+        (0.0, earth_iv*0.05, earth_iv*0.1)
         ), 
     Body(earth_m,
         (earth_sun_d, 0.0, 0.0), 
-        (0.0, earth_iv, 0.0)
-        ), 
-    Body(earth_m,
-        ((11/10)*earth_sun_d, 0.0, 0.0), 
-        (0.0, (11/10)*earth_iv, 0.0)
+        (0.0, -earth_iv, 0.0)
         )
+    # Body(earth_m,
+        # ((11/10)*earth_sun_d, 0.0, 0.0), 
+        # (0.0, (11/10)*earth_iv, 0.0)
+        # )
     ])
 
 sys2 = np.array([
@@ -115,28 +123,30 @@ sys4 = np.array([
         (0.0, 0.0, 0.0)
         ),
     Body(sun_m,
-        (9*earth_sun_d, 0.0, 0.0),
+        (-4*earth_sun_d, -2*earth_sun_d, 0.0),
         (0.0, 0.0, 0.0)
         ) 
     ])
 
 
-bodies = sys4
-xlim=(-10*earth_sun_d, 10*earth_sun_d)
+bodies = earth_sun_sys
+xlim=(-earth_sun_d, earth_sun_d)
 ylim=xlim
 zlim=xlim
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d',
+ax = fig.add_subplot(111, projection='3d', 
         xlim=xlim,
         ylim=ylim,
         zlim=zlim)
-graph = ax.scatter([a.pos[0] for a in bodies], [a.pos[1] for a in bodies], [a.pos[2] for a in bodies])
+graph = ax.scatter([a.pos[0] for a in bodies], [a.pos[1] for a in bodies], [a.pos[2] for a in bodies], s=100)
+com = calc_COM(bodies)
+com_graph = ax.scatter(com[0], com[1], com[2])
 title = ax.set_title('N-Body Sim')
 step = 60*60
-time = 60*60*24*30*12
+time = 60*60*24*5
 
-speed_multiplier = 200
+speed_multiplier = 160
 def update(i):
 
     global bodies 
@@ -150,24 +160,19 @@ def update(i):
                [a.pos[2] for a in bodies])
 
     graph._offsets3d = content
+    comx, comy, comz = calc_COM(bodies)
+    com_graph._offsets3d = ([comx],[comy],[comz])
     title.set_text('N-Body Sim, time={}'.format(i))
+
+    ax.view_init(elev=20., azim=0.5*i)
 
 
 
 if __name__=="__main__":
-    # b1pos = []
-    # b2pos = []
-    # b3pos = []
-    # for i in range(int(time/step)):
-        # b1pos.append(bodies[0].pos)
-        # b2pos.append(bodies[1].pos)
-        # b3pos.append(bodies[2].pos)
-        # update_body_list(bodies)
-    # ax.plot([a[0] for a in b1pos], [a[1] for a in b1pos], color="blue")
-    # ax.plot([a[0] for a in b2pos], [a[1] for a in b2pos], color="red")
-    # ax.plot([a[0] for a in b3pos], [a[1] for a in b3pos], color="green")
-    # plt.show()
     
     ani = animation.FuncAnimation(fig, update, frames=int(time/step),
                               interval=20, blit=False)
+    Writer = animation.writers['imagemagick']
+    writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+    ani.save('nbody.gif', writer=writer)
     plt.show()
